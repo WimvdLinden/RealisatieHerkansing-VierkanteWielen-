dcf1212cbc10ed7bd96611a7a77a5a1ea1cb32d0
WimvdLinden <57991373+WimvdLinden@users.noreply.github.com>
2023-11-08 15:49:20 +0100

    registreren aangemaakt
    aanmelden toegevoegd maar werkt gelaas niet(moet het nog even fixen)

---
+++ registreren.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'Partial/header.php';
+
+
+
+if(isset($_POST['register'])){
+    echo $user->register($_POST);
+}
+if(isset($_SESSION['loggedIn'])){
+    if($_SESSION['loggedIn']){
+        header("Location: admin.php");
+    }
+}
+
+?>
+
+    <form method='post'>
+        username:<input type="text" name="username">
+        password:<input type="password" name="password">
+        password conf:<input type="password" name="password-conf">
+        <input type="submit" name="register" value="Register">
+    </form>
